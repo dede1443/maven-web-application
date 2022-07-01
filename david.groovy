ddd4d2e0e4b8c12dc860db6af33f495e449cd6b3
dede1443 <105734852+dede1443@users.noreply.github.com>
2022-07-01 10:26:17 -0400

    Update david.groovy

--- david.groovy
+++ david.groovy
@@ -1,1 +1,15 @@
-
+def call(String stageName){
+  
+  if ("${stageName}" == "build")
+     {
+       sh "mvn clean package"
+     }
+  else if ("${stageName}" == "code quality")
+     {
+       sh "mvn clean sonar:sonar"
+     }
+  else if ("${stageName}" == "artifacts")
+     {
+       sh "mvn clean deploy"
+     }
+}
